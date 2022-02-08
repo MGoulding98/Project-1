b1f9996cf692bfc4a4e420cfd978d81137759bdb
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-02-07 20:22:41 -0700

    * Project 1.csproj: installed entityframeworkcore
    * TaskContext.cs: added content to the task context file for linking
      models

--- Models/TaskContext.cs
+++ Models/TaskContext.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Project_1.Models
 {
-    public class TaskContext
+    public class MovieContext : DbContext
     {
+        public MovieContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+            // leave blank for now
+        }
+
+        public DbSet<TaskModel> Tasks { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
