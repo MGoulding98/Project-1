6686eab3fb75fb4e54ab906678c5b79af4e90d80
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-02-07 20:18:54 -0700

    input the data for models

--- Models/TaskModel.cs
+++ Models/TaskModel.cs
@@ -1,11 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
-namespace Project_1.Models
+namespace Mission_6.Models
 {
     public class TaskModel
     {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public int Quadrant { get; set; }
+
+        // set up foreign key relationship
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public bool Completed { get; set; }
     }
 }
