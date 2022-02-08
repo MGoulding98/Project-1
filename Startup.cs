0130667b553f53c8803878163fa832628f38845c
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-02-07 21:01:53 -0700

    fixed a models import issue

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Project_1.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
