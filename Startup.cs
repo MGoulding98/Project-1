ed69a9a168643698feeeb9d5110267caa31e226c
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-02-08 18:35:56 -0700

    changed it back

--- Startup.cs
+++ Startup.cs
@@ -58,7 +58,7 @@ namespace Project_1
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{taskid?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
